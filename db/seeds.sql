be77b46217e97ecf0ba5b1a2bdc588a9473c5760
atulsingla82 <Lucky@Atuls-MacBook-Pro.local>
2017-05-15 21:46:23 -0400

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Jersey Burger');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name, devoured) VALUES ('Double Cheese', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('California Burger', true);
